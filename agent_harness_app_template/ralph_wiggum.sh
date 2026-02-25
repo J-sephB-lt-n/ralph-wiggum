e0a9f1728d96ea630074fbe843e5f0c0eca68341
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2026-02-25 11:58:09 +0200

    feat(ralph_wiggum.sh): input args working nicely

--- agent_harness_app_template/ralph_wiggum.sh
+++ agent_harness_app_template/ralph_wiggum.sh
@@ -3,31 +3,44 @@
 MAX_N_LOOPS=""
 AGENT_LIB=""
 
-while getopts ":l:a" opt; do
-  case &opt in
-    l)
-      MAX_N_LOOPS="$OPTARG"
-      ;;
-    a)
-      AGENT_LIB="$OPTARG"
-    :)
-      echo "ERROR: -$OPTARG requires a value"
-      exit 1
-      ;;
-    \?)
-      echo "ERROR: Invalid option -$OPTARG"
-      exit 1
-      ;;
-  esac
+while getopts ":l:a:" opt; do
+	case $opt in
+	l)
+		MAX_N_LOOPS="$OPTARG"
+		;;
+	a)
+		AGENT_LIB="$OPTARG"
+		;;
+	:)
+		echo "ERROR: -$OPTARG requires a value"
+		exit 1
+		;;
+	\?)
+		echo "ERROR: Invalid option -$OPTARG"
+		exit 1
+		;;
+	esac
 done
 
 if [[ -z "$MAX_N_LOOPS" ]]; then
-  echo 'ERROR: Argument -l (max n agent loops) is required.'
-  exit 1
+	echo 'ERROR: Argument -l (max n agent loops) is required.'
+	exit 1
 fi
 
 if [[ -z "$AGENT_LIB" ]]; then
-  echo 'ERROR: Argument -a (agent library) is required'
-  exit 1
+	echo 'ERROR: Argument -a (agent library) is required'
+	exit 1
 fi
 
+case "$AGENT_LIB" in
+cursor | opencode)
+	echo "Using agent library [ $AGENT_LIB ]"
+	;;
+*)
+	echo "Invalid option -a '$AGENT_LIB'. Valid options are ['cursor','opencode']"
+	exit 1
+	;;
+esac
+
+echo "MAX_N_LOOPS: $MAX_N_LOOPS"
+echo "AGENT_LIB:   $AGENT_LIB"
